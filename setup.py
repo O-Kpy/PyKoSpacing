def6550ed9d2998844491f40b46bed985711d0f5
O-Kpy <81207890+O-Kpy@users.noreply.github.com>
2023-02-06 11:35:30 +0900

    Update setup.py
    change tensorflow version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(name='pykospacing',
       include_package_data=True,
 
       install_requires=[
-          'tensorflow == 2.9.3',
+          'tensorflow == 2.11.0',
           'h5py == 3.1.0',
           'argparse >= 1.4.0',
       ],
